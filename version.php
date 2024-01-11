26ed06729e45f101cdffbebac7fa0ed91f76fe21
Jonas Lahme <jonas.lahme@joneva.de>
2024-01-11 12:45:07 +0100

    Updated Course Tile Layout
    Course tile layout has been adjusted

--- version.php
+++ version.php
@@ -3,6 +3,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023121800;
+$plugin->version   = 2023121801;
 $plugin->requires  = 2022111811;
 $plugin->component = 'block_course_explorer';
